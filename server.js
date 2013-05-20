ce668139e107fc2da8ac4462eb0baf83b9ed0fc4
Will <will@will-ThinkPad-SL.(none)>
2013-05-20 16:59:18 +0100

    Tidying public static assets

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.get('/', function(req, res) {
   res.end("<head><script src=\"//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js\"></script><script src=\"/app.js\"></script></head><body><div class=\"widget\"></div></body>");
 });
 
-app.use(express.static(__dirname+'/lib/images'));
-app.use(express.static(__dirname+'/lib/ft-velcro/distribution'));
-app.use(express.static(__dirname+'/lib/ft-velcro/font/ft-velcro'));
-app.use(express.static(__dirname+'/lib/ft-velcro/img/ft-velcro'));
+app.use(express.static(__dirname+'/public'));
 
 
 app.listen(3000, function() {
