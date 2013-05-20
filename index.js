17a01a37608157d3abb5924347dd84d3bd1ccc46
jamie <jamie@ft.com>
2013-05-20 19:42:41 +0100

    FT wrapper integration.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var Picknmix = require("./lib/picknmix");
+var Picknmix  = require("./lib/picknmix");
+var FTWrapper = require("./lib/utils/ft-wrapper");
 
 window.onload = function() {
   Picknmix.init();
+  FTWrapper.hack();
 };
 
 // Expose as global object
